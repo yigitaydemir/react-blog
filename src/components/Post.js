826faaead512ce6c10cfab62fbc12e40a98961c4
Yusuf Yiğit Aydemir <yusufyigitaydemir@gmail.com>
2023-06-02 21:38:02 +0300

    devam

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { Card, Button } from "flowbite-react";
 
 const Post = () => {
   const params = useParams();
@@ -12,17 +13,29 @@ const Post = () => {
       .then((response) => response.json())
       .then((data) => setDetails(data));
 
-      console.log(details)
+    console.log(details);
   }, []);
 
   return (
     <div>
-    {Object.keys(details || {}).map((key) => (
-      <p key={key}>
-        {key}: {details[key]}
-      </p>
-    ))}
-  </div>
+      {
+        <Card >
+          <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+            <p>{details && details["title"]}</p>
+          </h5>
+          <p className="font-normal text-gray-700 dark:text-gray-400">
+            <p>{ details && details["body"]}</p>
+          </p>
+          <Button color="warning">
+            <p>Edit</p>
+          </Button>
+
+          <Button color="failure">
+            <p>Delete</p>
+          </Button>
+        </Card>
+      }
+    </div>
   );
 };
 
